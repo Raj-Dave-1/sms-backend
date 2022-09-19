d0f57a4cbd121bc27476a01ebacc698b7289df99
Raj Dave <89149239+Raj-Dave-1@users.noreply.github.com>
2022-09-19 14:03:46 +0530

    Create RestController.js

---
+++ src/test/java/RestController.js
@@ -0,0 +1,14 @@
+
+package in.ac.charusat.studentmgmtsystem.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyDetails implements Details {
+
+}
